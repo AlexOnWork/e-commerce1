e9d35d3c05887d051be84c7d8e47ca450059273e
pictolin <sr.pictolin@gmail.com>
2024-03-21 23:44:19 +0100

    loqueseaya

---
+++ src/components/Guitar.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+const Guitar = ({cart,setCart, guitar}) => {
+
+    const{price,id,description,image,name}= guitar
+    
+   const handleClick=(guitar) =>{
+
+            setCart([...cart,guitar])
+   
+
+   }
+
+  return (
+    
+       <div className="col-md-6 col-lg-4 my-4 row align-items-center" id={id}>
+                <div className="col-4">
+                    <img className="img-fluid" src={`./public/img/${image}.jpg`} alt="{imagen guitarra}" />
+                </div>
+                <div className="col-8">
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p className="fw-black text-primary fs-3">${price}</p>
+                    <button 
+                        type="button"
+                        className="btn btn-dark w-100"
+                        onClick={()=>handleClick(guitar)}
+                    >Agregar al Carrito</button>
+                </div>
+            </div>
+    
+  )
+}
+
+export default Guitar
